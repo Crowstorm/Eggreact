e871898ae1c1695c796cb2b929223ad54b66d94e
Crowstorm <crowstormofficial@gmail.com>
2017-09-05 16:05:48 +0200

    podstawy

--- eggreact/src/App.js
+++ eggreact/src/App.js
@@ -1,5 +1,36 @@
 import React from 'react';
 
-const App = () => <h1>Hello</h1>
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      text: 'this is the state text'
+    }
+  }
+
+  update(event){
+    this.setState({text: event.target.value})
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>{this.state.text}</h1>
+        <input type='text'
+        onChange={this.update.bind(this)} />
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: 'this is the default text'
+}
+
 
 export default App
