b203baf3cd8ca3fb4237abb982035935ce4eaca0
Crowstorm <crowstormofficial@gmail.com>
2017-09-06 09:30:45 +0200

    basics

--- eggreact/src/App.js
+++ eggreact/src/App.js
@@ -1,36 +1,89 @@
 import React from 'react';
 
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      text: 'this is the state text'
+class App extends React.Component{
+  render(){
+    return <Title />
+  }
+}
+
+const Title = (props) => (
+  <h1>Title: {props.text}</h1>
+)
+
+Title.propTypes = {
+  text(props, propName, component){
+    if(!(propName in props)){
+      return new Error(`missing ${propName}`)
+    }
+    if(props[propName].length < 6){
+      return new Error (`${propName} was too short`);
     }
   }
-
-  update(event){
-    this.setState({text: event.target.value})
-  }
-
-  render(){
-    return (
-      <div>
-        <h1>{this.state.text}</h1>
-        <input type='text'
-        onChange={this.update.bind(this)} />
-      </div>
-    )
-  }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
 
-App.defaultProps = {
-  txt: 'this is the default text'
-}
 
+
+
+
+
+
+
+
+// class App extends React.Component {
+//   render(){
+//     return <button>I <Heart />React</button>
+//   }
+// }
+
+// const Button = (props) => {
+//   <button>{props.children}</button>
+// }
+
+// class Heart extends React.Component{
+//   render(){
+//     return <span>&hearts;</span>
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class App extends React.Component {
+//   constructor(){
+//     super();
+//     this.state = {
+//       text: 'this is the state text'
+//     }
+//   }
+
+//   update(event){
+//     this.setState({text: event.target.value})
+//   }
+
+//   render(){
+//     return (
+//       <div>
+//         <h1>{this.state.text}</h1>
+//         <Widget update={this.update.bind(this)} />
+//       </div>
+//     )
+//   }
+// }
+
+// const Widget = (props) => (        
+// <input type='text' onChange={props.update} />
+//)
 
 export default App
